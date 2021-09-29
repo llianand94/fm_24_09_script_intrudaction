83dc19adc8d0bc928aa747db7635545abdf46db1
Andrey Shapovalov <llianandr94@gmai.com>
2021-09-29 11:47:45 +0300

    Create square equation !!! nice

--- index.js
+++ index.js
@@ -1,38 +1,26 @@
-let firstNum = 45;
-let secondNum = 12;
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @param {number} c 
+ * @returns { null ||  array[num1, num2] }
+ */
 
-console.log(firstNum + secondNum);
+const solveSquareEquation = function (a, b, c){
+ const D = b*b - 4*a*c;
+ if (D < 0){
+   return null
+ } 
+ if(D===0){
+   const x = (-b)/ (2*a);
+   return [x, x];
+ }
+ const x1 = (-b + D**0.5) / (2*a);
+ const x2 = (-b - D**0.5) / (2*a);
+ return [x1, x2];
 
-console.log(firstNum - secondNum);
+}
 
-console.log(firstNum * secondNum);
-
-console.log(firstNum / secondNum);
-
-console.log(firstNum % secondNum);
-
-let specialNumber = 101;
-let booleanValue = true;
-let currentLanguage = 'Java';
-let userName = 'Elon Musk';
-let emptyValue = null;
-
-
-
-let number = 23;
-
-console.log('postf increment= ', number++);
-number = 23; 
-console.log('postf decrement= ', number--);
-number = 23;
-console.log('pref increment= ', ++number);
-number = 23;
-console.log('pref decrement= ', --number);
-
-number += 11;
-number /= 2;
-number -= 1;
-number *= 11;
-number += 11;
-number %= 5;
-
+console.log(solveSquareEquation(-1,-2,15));
+console.log(solveSquareEquation(1,12,36));
+console.log(solveSquareEquation(5,3,7));
