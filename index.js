59e5793db9596ff79fc986149eb6dc1cb251424e
Andrey Shapovalov <llianandr94@gmai.com>
2021-10-04 12:15:17 +0300

    Add coffeeMachine

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
-function Users(name, sname, age, isAdmin){
-  this.name = name;
-  this.sname = sname;
-  this.age = age;
-  this.isAdmin = isAdmin;
-  this.fullname = function(){
-     return this.name + '  ' + this.sname;
-  }
+function CoffeeMachine(brand= 'Ford', maxVolume){
+  this.brand = brand;
+  this.maxVolume = maxVolume; //ml
+  this.volume = 0;
+  this.inOn = false;
+
+  this.addWater = function(value){
+    if(this.isOn){
+      if(value<0 || value+ this.volume >=this.maxVolume){
+        return false;
+      };
+     return this.volume += value;
+    }    
+  };
+
+  this.makeCoffee = function(value){
+    if(value<0|| this.volume-value<0){
+      return false;
+    };   
+    return this.volume -= value;
+  };
+
+ if(this.isOn === false){
+   return null;
+ };
+  this.turnOn = function(value){
+    
+    // if(typeof(Boolean(value))){
+
+    // }
+      
+  };
+  
+
 };
-
-const artem = new Users('Artem', 'Incognito', 18, true);
-const ivan = new Users('Ivan', 'Incognito', 20, false);
-
-console.log(artem.fullname());
+const test = new CoffeeMachine(undefined, 150);
