35726bf99fd6246fa3edc0df639aad4fe22714ff
Andrey Shapovalov <llianandr94@gmai.com>
2021-10-05 11:11:22 +0300

    Reverse quotes

--- index.js
+++ index.js
@@ -1,36 +1,11 @@
-function CoffeeMachine(brand= 'Ford', maxVolume=150){
-  this.brand = brand;
-  this.maxVolume = maxVolume; //ml
-  this.volume = 0;
-  this.isOn = false;
+const user = {
+  firstName: 'Elon',
+  lastName: 'Mask',
+  age: 50,
+  isMale: true,
+}
+const sayHello = function(obj){
+ return `Hello, ${obj.firstName} ${obj.lastName}!`;
 
-  this.addWater = function(value){
-    
-      if(value<0 || value+ this.volume >=this.maxVolume){
-        return false;
-      };
-     return this.volume += value;
-    };    
-  
-
-  this.makeCoffee = function(value){
-    if(this.isOn){ 
-      if(value<0|| this.volume-value<0){
-        return false;
-      }
-      return this.volume -= value;     
-    }
-    return false;    
-  };
-
- 
-  this.turnOn = function(){
-    if(this.isOn){
-      return this.isOn = false;      
-    } 
-    return this.isOn = true
-  };
-  
-
-};
-const test = new CoffeeMachine('Yacobs', 160);
+}
+console.log(sayHello(user));
