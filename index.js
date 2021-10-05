c24b89c6c640898d44bb75e50caeae7719e77cca
Andrey Shapovalov <llianandr94@gmai.com>
2021-10-05 12:04:56 +0300

    add exsample  function to find by operator access p

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const user = {
-  firstName: 'Elon',
-  lastName: 'Mask',
-  age: 50,
-  isMale: true,
-}
-const sayHello = function(obj){
- return `Hello, ${obj.firstName} ${obj.lastName}!`;
+// const table = {
+//   '12 * 2 =': 24,
+// }
 
+const createMultiplicationTable = function(min=1, max=10) {
+  const table = {};
+  for(let i = min; i<=max; i++){
+    for(let j = 1; j<=max; j++){
+      table[`${i} * ${j} = `] = i*j; 
+    }
+  }
+  return table;
 }
-console.log(sayHello(user));
+
+
+const checkObject = function(str, obj){ 
+  return obj[str]; 
+}
+
+const str = '2 * 2 = ';
+const table = createMultiplicationTable();
+
+console.log(checkObject(str, table));
