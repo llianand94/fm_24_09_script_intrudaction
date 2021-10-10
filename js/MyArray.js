696d5758bb87802fedddea0b92334f95aa4dbac7
Andrey Shapovalov <llianand94@gmail.com>
2021-10-10 21:13:04 +0300

    Add forEach method

---
+++ js/MyArray.js
@@ -0,0 +1,40 @@
+/* data */
+function MyArray(){
+  this.length = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    this.push(arguments[i]);
+  }
+}
+/* logic */
+function MyArrayProto(){
+  this.push = function(){ //1,2,3,4,5,8
+    for (let i = 0; i < arguments.length; i++) {
+      this[this.length++] = arguments[i];
+    }
+    return this.length;
+  }
+  this.pop = function(){
+    if(this.length===0){
+      return;
+    }
+    const item = this[--this.length];
+    delete this[this.length];
+    return item;
+  }
+  this.forEach = function(func){
+    for(let i=0; i<this.length;i++){
+      func(this[i]);
+    }
+  }
+}
+
+
+
+/* Prototype */
+MyArray.prototype = new MyArrayProto();
+
+
+const myArray = new MyArray(1,1,1,15,5);
+myArray.push(2,2,2);
+myArray.pop();
+console.log(myArray);
