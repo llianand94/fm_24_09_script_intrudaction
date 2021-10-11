8bc7aeb2cc9dda6669ad4e104af756e4d9142ab2
Andrey Shapovalov <llianandr94@gmai.com>
2021-10-11 11:58:24 +0300

    Add example recurcive func

--- js/index.js
+++ js/index.js
@@ -1,11 +1,19 @@
 'use strict';
 
-const sum = function(...arr){
-  let result = null;
-  
-  arr.forEach((n)=>{
-    result += n;
-  })
-  return result;
+const degree = function(num, exp){
+  if(exp===1){
+    return num;
+  }
+
+  return num* degree(num, exp-1)
 }
-sum(1,2,3,4);
+const factorial = function(num){
+  
+  if(num===0){
+    return num = 1;
+  }
+  return num * factorial(num-1)
+}
+
+console.log(factorial(5));
+
