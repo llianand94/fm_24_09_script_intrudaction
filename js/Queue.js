410b9a58e2fe628e79346b28822d755484d3bbe4
Andrey Shapovalov <llianand94@gmail.com>
2021-10-24 13:47:28 +0300

    Add func to merge two Queues

--- js/Queue.js
+++ js/Queue.js
@@ -27,6 +27,20 @@ class Queue{
 //написать функция сливает поочередно две очереди в одну новую
 const mergeQueues = (q1, q2) => {
   const q3 = new Queue();
-  //дописать
-  return q3;
+  const maxSizeQ=  q1.size>q2.size? q1.size: q2.size; 
+  for(let i =0; i<maxSizeQ;i++){ 
+    const unshiftItemQ1 = q1.unshift();
+    if(unshiftItemQ1!==undefined){
+       q3.push(unshiftItemQ1);
+    } 
+    const unshiftItemQ2 = q2.unshift();
+    if(unshiftItemQ2!==undefined){
+    q3.push(unshiftItemQ2);
+    }
+   }
+    return q3;
 }
+const qu1 = new Queue(1,2,3,4,5,6);
+const qu2 = new Queue(8,9);
+
+// console.log(mergeQueues(qu1, qu2));
